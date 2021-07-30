a848a6b5bbce2eb32235384c985c44ced8a96b6c
Croquignolex <gpetitalex10@gmail.com>
2021-07-30 17:46:42 +0100

    Save controller

--- app/Http/Controllers/API/FlotageController.php
+++ app/Http/Controllers/API/FlotageController.php
@@ -36,8 +36,9 @@ class FlotageController extends Controller
         $agent = Roles::AGENT;
         $recouvreur = Roles::RECOUVREUR;
         $superviseur = Roles::SUPERVISEUR;
+        $controlleur = Roles::CONTROLLEUR;
         $ges_flotte = Roles::GESTION_FLOTTE;
-        $this->middleware("permission:$recouvreur|$superviseur|$ges_flotte|$agent");
+        $this->middleware("permission:$recouvreur|$superviseur|$ges_flotte|$agent|$controlleur");
     }
 
     /**
