499d76c79cb53dd0e41608aebdc3032f828e7014
Croquignolex <gpetitalex10@gmail.com>
2021-07-30 12:44:34 +0100

    Auhtorize overserr

--- app/Http/Controllers/API/VendorController.php
+++ app/Http/Controllers/API/VendorController.php
@@ -18,8 +18,9 @@ class VendorController extends Controller
         $agent = Roles::AGENT;
         $recouvreur = Roles::RECOUVREUR;
         $superviseur = Roles::SUPERVISEUR;
+        $controlleur = Roles::CONTROLLEUR;
         $ges_flotte = Roles::GESTION_FLOTTE;
-        $this->middleware("permission:$recouvreur|$superviseur|$ges_flotte|$agent");
+        $this->middleware("permission:$recouvreur|$superviseur|$ges_flotte|$agent|$controlleur");
     }
 
     /**
